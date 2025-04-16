81906fce2b6c4db9e7a29fe917091651e6dd8edc
HShrigma <hristostantchev@abv.bg>
2025-04-16 14:00:54 +0300

    Update JankServer.java - run server, add simple response; Add test Maven build

--- src/main/java/server/JankServer.java
+++ src/main/java/server/JankServer.java
@@ -1,6 +1,22 @@
 package server;
 
-public class JankServer {
+import java.io.*;
+import java.net.*;
 
-    
+public class JankServer {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting Server...");
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started. Try: curl http://localhost:8080");
+            // Accept ONE connection
+            Socket clientSocket = serverSocket.accept();
+
+            OutputStream out = clientSocket.getOutputStream();
+            out.write("HTTP/1.1 200 OK\r\n\r\nHello from raw Java!".getBytes());
+
+            //clean up
+            clientSocket.close();
+        }
+
+    }
 }
