6da2b452f244cb73c4dca36b5c50ad1c0871d6e0
HShrigma <hristostantchev@abv.bg>
2025-04-16 15:21:29 +0300

    Update JankServer.java - add single thread multiple request handling with req limit

--- src/main/java/server/JankServer.java
+++ src/main/java/server/JankServer.java
@@ -2,23 +2,33 @@ package server;
 
 import java.io.*;
 import java.net.*;
+
 import server.*;
 
 public class JankServer {
     public static void main(String[] args) throws IOException {
         HTTPParser.PrintServerSideInit();
-        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+        int maxRequests = 2;
+        int reqsCount = 0;
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server started. Try: curl http://localhost:8080");
 
-            System.out.println("Server started. Try: curl http://localhost:8080");
-            // Accept ONE connection
-            Socket clientSocket = serverSocket.accept();
+        while (reqsCount < maxRequests) {
+            // Accept connections on 1 thread using try w/ resources
+            try (Socket clientSocket = serverSocket.accept()) {
+                OutputStream out = clientSocket.getOutputStream();
+                
+                String res = HTTPParser.GetResponse200("Hello from raw Java using HTTPParserClass!\n");
+                
+                out.write(res.getBytes());
 
-            OutputStream out = clientSocket.getOutputStream();
-            out.write(HTTPParser.GetResponse200("Hello from raw Java using HTTPParserClass!\n").getBytes());
+            } catch (IOException e) {
+                System.err.println("Request failed: " + e.getMessage());
+            }
 
-            //clean up
-            clientSocket.close();
+            reqsCount++;
         }
-
+        System.out.println("Reached max Requests: " + maxRequests + "\nServer is closing");
+        serverSocket.close();
     }
 }
